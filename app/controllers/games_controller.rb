5a9244519fd0a520a4471710059dcee1e9ed944e
Oceane BOUNAN <bounan.oceane@orange.fr>
2024-04-22 19:39:32 +0400

    Challenge completed

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,30 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    i = 0
+    @letters = []
+    while i < 10
+      @letters << ('A'..'Z').to_a.sample(1)
+      i += 1
+    end
+    @letters
+  end
+
+  def score
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:answer]}"
+    html_doc = JSON.parse(URI.open(url).read)
+    @letters = JSON.parse(params[:letters])
+    session[:result] = 0 if session[:result].nil?
+    session[:result] += (html_doc['length']).to_i
+    @session_result = session[:result]
+    @score = if html_doc['found'] == false
+               "Sorry but #{params[:answer].upcase} does not seem to be a valid English word"
+             elsif @letters.flatten.any? { |letter| params[:answer].upcase.include? letter }
+               "Congratulations! #{params[:answer].upcase} is a valid English word.
+               Your score is #{(html_doc['length']).to_i}"
+             else
+               "Sorry but #{params[:answer].upcase} can not be built out of #{@letters.flatten.join(', ')}"
+             end
+  end
+end
